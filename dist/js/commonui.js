d9cd48506e3b1c7ea474ba69640b011cf48ae722
kogwing <kogwing@gmail.com>
2021-03-21 21:52:31 +0900

    update

--- dist/js/commonui.js
+++ dist/js/commonui.js
@@ -1,6 +1,29 @@
 $(function(){
 
 
+	// input disabled true / false
+	//disChk();
+
 
     //script
 })
+
+
+
+
+
+
+// input disabled true / false
+function disChk(){
+	var $disChk = $('[.dischk');
+    $disChk.on('change', function() {
+        var data = $(this).data('dischk');
+        if ( $(this).prop('checked') )
+        {
+            $('[data-disabled="'+ data +'"]').prop('disabled',false);
+            
+        } else {
+            $('[data-disabled="'+ data +'"]').prop('disabled',true);
+        }
+    });
+}
