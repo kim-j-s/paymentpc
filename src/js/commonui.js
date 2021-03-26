db2f274a5f11b8bc15b4fb7d9ea11043931de4ff
kogwing <kogwing@gmail.com>
2021-03-26 17:12:47 +0900

    up

--- src/js/commonui.js
+++ src/js/commonui.js
@@ -1,3 +1,154 @@
+// input disabled true / false
+function disChk(){
+    var $disChk = $('.dischk');
+    var $disChkOff = $('.dischk-off');
+    $disChk.on('change', function() {
+        var data = $(this).data('dischk');
+        if ( $(this).prop('checked') )
+        {
+            $('[data-disabled="'+ data +'"]').prop('disabled',false);
+        } else {
+            $('[data-disabled="'+ data +'"]').prop('disabled',true);
+        }
+    });
+
+    $disChkOff.on('change', function() {
+        var data = $(this).data('dischk');
+        if ( $(this).prop('checked') )
+        {
+            $('[data-disabled="'+ data +'"]').prop('disabled',true);
+        }
+    });
+}
+
+// 파일첨부
+var $fileBox = null;
+function fileInit(){
+    $fileBox = $('.inp-file');
+    fileLoad();
+}
+function fileLoad(){
+    $.each($fileBox, function(idx){
+        var $this = $fileBox.eq(idx),
+        $btnUpload = $this.find('[type="file"]'),
+        $label = $this.find('.file-label');
+        $btnUpload.on('change', function(){
+            var $target = $(this),
+            fileName = $target.val();
+            $fileText = $target.closest('.inp-file').find('.file-name');
+            $fileText.val(fileName);
+        });
+        $btnUpload.on('focusin focusout', function(e){
+            e.type == 'focusin' ? $label.addClass('file-focus') : $label.removeClass('file-focus');
+        });
+    });
+}
+
+// dp select
+function dpSelect() {
+    var $dps = $('.dp-select');
+    var $dpnone = $('.dp-trg');
+    $dps.on('change', function(){
+        var val = $(this).val();
+        console.log(val);
+        if (val == 'display') {
+            $dpnone.show();
+        } else {
+            $dpnone.hide();
+        }
+    })
+}
+
+// slide
+function slide() {
+    $('.slide-act').on('click', function(){
+        var data = $(this).data('slide');
+        var $trg = $("[data-slidecont='" + data + "']");
+
+        if ( $trg.css('display') == 'block' ){
+            $(this).addClass('on');
+            $trg.stop().slideUp(500);
+        } else {
+            $(this).removeClass('on');
+            $trg.stop().slideDown(500);
+        }
+    })
+}
+
+// auto Email
+function autoEmail() {
+    var $autoCont = $('.mail-list-cont');
+    var $autoEmail = $('.email-auto');
+    var $autoDomain = $('.mail-list a');
+    var etcWord = '직접선택';
+    var domains = [
+        "konai.com",
+        "naver.com",
+        "nate.com",
+        "gmail.com",
+        "hotmail.com",
+        "daum.net",
+        "yahoo.co.kr"
+    ];
+    $autoEmail.on('keyup', function(){
+        value = $(this).val();
+        if (value.indexOf("@") != -1) {
+            value = value.split("@")[0];
+            $autoEmail.addClass('on');
+            $autoCont.addClass('on');
+            var mailList = '';
+            var typing = "<li><a href='#'>" + etcWord + "</a></li>";
+            for (i = 0; i < domains.length; i++) {
+                mailList += "<li><a href='#'>" + value + "@" + domains[i] + "</a></li>";
+            }
+            $('.mail-list').html(typing + mailList);
+        } else {
+            $('.mail-list').empty();
+            $autoCont.removeClass('dp-none');
+            displayList();
+        }
+    });
+
+    $(document).on('click', '.mail-list li a', function(e){
+        e.preventDefault();
+        var value = $(this).html();
+        if ( value == etcWord ) {
+            $autoCont.addClass('dp-none');
+            $(this).closest('.input-text').find('input').focus();
+        } else {
+            $(this).closest('.input-text').find('input').val(value);
+            displayList();
+        }
+    });
+
+    emailDisplay();
+
+    function displayList() {
+        $autoEmail.removeClass('on');
+        $autoCont.removeClass('on');
+    }
+
+    function emailDisplay(){
+        $(document).on('mouseenter focus', '.mail-list a', function(){
+            $autoEmail.addClass('on');
+            $autoCont.addClass('on');
+        });
+        
+        $(document).on('blur', '.mail-list > li a:last, .mail-list > li', function(){
+            displayList();
+        });
+
+        $autoEmail.on('blur', function(){
+            setTimeout(function(){
+                if ( !$('.mail-list a').is(':focus') ){
+                    displayList();
+                }
+            }, 100);    
+        });
+    }
+}
+
+
 $(function(){
 
     //달력
@@ -42,172 +193,8 @@ $(function(){
     // auto Email
     autoEmail();
 
-
     //script
 })
 
 
 
-
-
-
-// input disabled true / false
-function disChk(){
-	var $disChk = $('.dischk');
-    var $disChkOff = $('.dischk-off');
-    $disChk.on('change', function() {
-        var data = $(this).data('dischk');
-        if ( $(this).prop('checked') )
-        {
-            $('[data-disabled="'+ data +'"]').prop('disabled',false);
-        } else {
-            $('[data-disabled="'+ data +'"]').prop('disabled',true);
-        }
-    });
-
-    $disChkOff.on('change', function() {
-        var data = $(this).data('dischk');
-        if ( $(this).prop('checked') )
-        {
-            $('[data-disabled="'+ data +'"]').prop('disabled',true);
-        }
-    });
-}
-
-
-// 파일첨부
-var $fileBox = null;
-function fileInit(){
-    $fileBox = $('.inp-file');
-    fileLoad();
-}
-function fileLoad(){
-    $.each($fileBox, function(idx){
-        var $this = $fileBox.eq(idx),
-        $btnUpload = $this.find('[type="file"]'),
-        $label = $this.find('.file-label');
-        $btnUpload.on('change', function(){
-            var $target = $(this),
-            fileName = $target.val();
-            $fileText = $target.closest('.inp-file').find('.file-name');
-            $fileText.val(fileName);
-        });
-        $btnUpload.on('focusin focusout', function(e){
-            e.type == 'focusin' ? $label.addClass('file-focus') : $label.removeClass('file-focus');
-        });
-    });
-}
-
-
-
-// dp select
-function dpSelect() {
-    var $dps = $('.dp-select');
-    var $dpnone = $('.dp-trg');
-    $dps.on('change', function(){
-        var val = $(this).val();
-        console.log(val);
-        if (val == 'display') {
-            $dpnone.show();
-        } else {
-            $dpnone.hide();
-        }
-    })
-
-}
-
-
-// slide
-function slide() {
-    $('.slide-act').on('click', function(){
-        var data = $(this).data('slide');
-        var $trg = $("[data-slidecont='" + data + "']");
-
-        if ( $trg.css('display') == 'block' ){
-            $(this).addClass('on');
-            $trg.stop().slideUp(500);
-        } else {
-            $(this).removeClass('on');
-            $trg.stop().slideDown(500);
-        }
-    })
-}
-
-
-// auto Email
-function autoEmail() {
-    var domains = [
-        "직접입력",
-        "konai.com",
-        "naver.com",
-        "nate.com",
-        "gmail.com",
-        "hotmail.com",
-        "daum.net",
-        "yahoo.co.kr"
-    ];
-
-    // Using DropDown element to suggest static list of domains
-    var EmailDomainSuggester = function ($bindTo) { 
-        var datalist = null;  
-
-        var init = function () {    
-            addElements();
-            bindEvents();
-        };
-      
-        var addElements = function () {
-            var datalistId = 'email_options_' + $bindTo.attr('class');
-
-            //create empty datalist
-            datalist = $("<datalist />", {
-                id: datalistId
-            }).insertAfter($bindTo);    
-
-            // correlate to input
-            $bindTo.attr("list", datalistId);
-        };
-      
-        var bindEvents = function () {
-            $bindTo.on("keyup", testValue);
-        };
-      
-        var testValue = function (event) {
-            var el = $(this),
-                value = el.val();
-                
-            // email has @
-            if (value.indexOf("@") != -1) {
-                value = value.split("@")[0];
-                addDatalist(value);
-            } else {
-                // empty list
-                emptyDatalist();
-            }
-        };
-      
-        var emptyDatalist = function () {
-            datalist.empty();
-        };
-      
-        var addDatalist = function (value) {
-            var i,
-                newOptionsString = '';
-
-            // loop over all the domains in our array
-            for (i=0; i<domains.length; i++) {
-            newOptionsString += "<option value='" +
-                            value + "@" +
-                            domains[i] + 
-                            "'>";
-            }
-
-            // add all the <option>s to the datalist
-            datalist.html(newOptionsString);
-        };  
-      
-        init();
-    };
-
-    var edsEmail = new EmailDomainSuggester($('.email-auto'));
-}
